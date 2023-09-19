b5ae711a9b40fe56397aa61e12f992cd396ef47a
Vitamax <vitamaxoduo@gmail.com>
2023-09-19 13:11:02 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,40 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+# from flask import Flask, request, current_app
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     host = request.headers.get('Host')
+#     appname = current_app.name
+#     return f"""<h1>The host for this page is {host}</h1>
+#             <h2>The name of this application is {appname}</h2>"""
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
+# Let's set up a hook so that our views all know where our application files are located:
+import os
+
+from flask import Flask,request, current_app, g, make_response
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
+    
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response_body =  f'''
+            <h1>The host for this page is {host}</h1>
+            <h2>The name of this application is {appname}</h2>
+            <h3>The path of this application on the user's device is {g.path}</h3>'''
+            
+    status_code = 200
+    headers = {}
+
+    return make_response(response_body, status_code, headers)
